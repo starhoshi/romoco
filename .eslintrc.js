96778666fe4103a17c63e2a0c6cf18a574307ebc
starhoshi <kensuke1751@gmail.com>
2022-11-18 11:38:17 +0900

    Add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,6 @@
 module.exports = {
-  extends: [
-    "next/core-web-vitals",
-    "standard",
-    "prettier",
-  ],
-  plugins: [
-    "@typescript-eslint",
-  ],
+  extends: ["next/core-web-vitals", "standard", "prettier", "plugin:storybook/recommended"],
+  plugins: ["@typescript-eslint"],
   rules: {
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "warn"
---
+++ .storybook/main.js
@@ -0,0 +1,54 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    'storybook-addon-next-router'
+  ],
+  "staticDirs": [{ from: '../public', to: '/' }],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
+  webpackFinal(config) {
+    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../src')]
+
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ]
+
+    const newConfig = {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [...(config.module.rules || [])],
+      },
+    }
+
+    newConfig.module.rules.find((rule) => rule.test.toString() === '/\\.css$/').exclude = /\.module\.css$/
+
+    newConfig.module.rules.push({
+      test: /\.module\.css$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+            modules: true,
+          },
+        },
+        'postcss-loader',
+      ],
+    })
+
+    return newConfig
+  },
+}
