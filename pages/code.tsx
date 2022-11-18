1819f47c5a705ca47166d07c18bc609df0646881
starhoshi <kensuke1751@gmail.com>
2022-11-18 15:32:41 +0900

    test

--- pages/code.tsx
+++ pages/code.tsx
@@ -1,16 +1,16 @@
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 import { Template } from '@/_core/components/Template'
 import { NextPage } from 'next'
 import { Code } from '@/code'
-import { Meta, siteTitle } from '@/_core/components/Meta'
+// import { Meta, siteTitle } from '@/_core/components/Meta'
 
 const CodePage: NextPage = () => {
-  const router = useRouter()
-  const query = router.query.string as string | undefined
+  // const router = useRouter()
+  // const query = router.query.string as string | undefined
 
   return (
     <>
-      <Meta title={`${siteTitle} - ${query}`} />
+      {/* <Meta title={`${siteTitle} - ${query}`} /> */}
       <Template>
         <Code />
       </Template>
