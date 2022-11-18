f908bc4b6709f44bf7dbf415f9f7173856314b1a
starhoshi <kensuke1751@gmail.com>
2022-11-18 15:50:13 +0900

    remove meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import { Meta } from '@/_core/components/Meta'
 import { Template } from '@/_core/components/Template'
 import { Top } from '@/top'
 import { NextPage } from 'next'
+import { Meta } from '@/_core/components/Meta'
 
 const TopPage: NextPage = () => {
   return (
